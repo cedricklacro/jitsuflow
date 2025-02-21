d90567be1b49006df47836cfbd40ad63cd3c6a31
Cedrick <lacro.cedrick@outlook.com>
2025-02-21 03:38:47 -0700

    Install react-modal and prepare paths on app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,23 @@ import NotesAddPage from './pages/notes/NotesAddPage/NotesAddPage';
 import NotesDetailsPage from './pages/notes/NotesDetailsPage/NotesDetailsPage';
 import NotesEditPage from './pages/notes/NotesEditPage/NotesEditPage';
 import NotFoundPage from './pages/others/NotFoundPage/NotFoundPage';
+import Header from './components/Header/Header';
+
 function App() {
 
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<UnderConstructionPage />} />
-        <Route path="/jitsuflow" element={<NotesMenuPage />} />
-        <Route path="/jitsuflow" element={<NotesAddPage />} />
-        <Route path="/jitsuflow/:noteid" element={<NotesDetailsPage />} />
-        <Route path="/jitsuflow/:noteid/edit" element={<NotesEditPage />} />
+        <Route path="/jitsujournal" element={<NotesMenuPage />} />
+        <Route path="/jitsujournal/add" element={<NotesAddPage />} />
+        <Route path="/jitsujournal/:noteid" element={<NotesDetailsPage />} />
+        <Route path="/jitsujournal/:noteid/edit" element={<NotesEditPage />} />
+        <Route path="/profile" element={<UnderConstructionPage />} />
+        <Route path="/flowbuilder" element={<UnderConstructionPage />} />
+        <Route path="/billing" element={<UnderConstructionPage />} />
+        <Route path="log-out" element={<UnderConstructionPage />} />
         <Route path="/not-found" element={<NotFoundPage />} />
         <Route path="*" element={<NotFoundPage />} /> 
       </Routes>
