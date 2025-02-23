5eee6ab43a33193233ea987b22bcc62e0103339c
Cedrick <lacro.cedrick@outlook.com>
2025-02-23 08:10:26 -0700

    Create placeholder for pages not yet created (under construction)

--- src/pages/others/UnderConstructionPage/UnderConstructionPage.jsx
+++ src/pages/others/UnderConstructionPage/UnderConstructionPage.jsx
@@ -1,10 +1,16 @@
 import './UnderConstructionPage.scss';
-
+import cat from '../../../assets/images/cat.png';
 function UnderConstructionPage() {
   return (
-    <div>
-      
-    </div>
+    <section className='under-construction'>
+      <h1 className='under-construction__header'>Page Under Construction</h1>
+      <main className='under-construction__main-container'>
+        <div className='under-construction__categories'>
+          <img className='under-construction__cat' src={cat}/>
+        </div>
+      </main>
+    </section>
+
   )
 }
 
