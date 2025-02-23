62ee427d5d9cf3f7bc377c03683c5717f091dece
Cedrick <lacro.cedrick@outlook.com>
2025-02-23 13:32:15 -0700

    Create Notes Add functionality (no styling)

--- src/pages/notes/NotesAddPage/NotesAddPage.jsx
+++ src/pages/notes/NotesAddPage/NotesAddPage.jsx
@@ -1,11 +1,47 @@
 import './NotesAddPage.scss';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import PageHeader from '../../../components/PageHeader/PageHeader';
+import NotesForm from '../../../components/NotesForm/NotesForm';
 
 function NotesAddPage() {
+  const [itemData, setItemData] = useState({
+    title: '',
+    category_id: '',
+    content: '',
+    tags: [],
+    paths: {
+      entry: [],
+      exit: [],
+      counter_for: [],
+      can_be_countered_by: []
+    }
+  });
+
+  const baseUrl = import.meta.env.VITE_BASE_URL;
+  const navigate = useNavigate();
+
+  const handleSubmit = async (newNote) => {
+    try {
+      const response = await axios.post(`${baseUrl}/notes`, newNote);
+      navigate(`/jitsujournal/${response.data.note_id}`);
+    } catch (error) {
+      console.error('Error adding note:', error);
+    }
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
+    <section className="add-note">
+      <PageHeader title="Add New Note" type="withback" />
+      <NotesForm
+        itemData={itemData}
+        setItemData={setItemData}
+        handleSubmit={handleSubmit}
+        submitButtonText="Add Note"
+      />
+    </section>
+  );
 }
 
-export default NotesAddPage
+export default NotesAddPage;
