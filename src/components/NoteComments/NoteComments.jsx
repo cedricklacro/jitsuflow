c4887fe44e0d534dff446288ed7e843377f8e376
Cedrick <lacro.cedrick@outlook.com>
2025-02-23 08:14:54 -0700

    Create note comments component (no styling)

---
+++ src/components/NoteComments/NoteComments.jsx
@@ -0,0 +1,43 @@
+import './NoteComments.scss'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { formatDate } from '../../utils/utils';
+
+function NoteComments({noteid}) {
+    const [comments, setComments] = useState([]);
+    const baseUrl = import.meta.env.VITE_BASE_URL;
+
+    const getComments = async () => {
+        try {
+            const response = await axios.get(`${baseUrl}/notes/${noteid}/comments`);
+            setComments(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+  useEffect(() => {
+    getComments();
+  }, []);
+
+  return (
+    <section className="comments">
+      <span className="comments__count">
+        {`${comments.length} ${comments.length === 1 ? "Comment" : "Comments"}`}
+      </span>
+      {comments.map((comment) => (
+        <div className="comment" key={comment.comment_id}>
+          <div className="comment__toprow">
+            <span className="comment__info">{comment.username}</span>
+            <span className="comment__info">{formatDate(comment.created_at)}</span>
+            <span className="comment__info">{comment.category}</span>
+          </div>
+          <span className="comment__comment">{comment.comment}</span>
+        </div>
+      ))}
+    </section>
+
+  )
+}
+
+export default NoteComments
