e81a3f51f8da848c040c6e5d26d60ae151731a29
Cedrick <lacro.cedrick@outlook.com>
2025-02-23 18:43:03 -0700

    Add styling for notes menu page

--- src/pages/notes/NotesMenuPage/NotesMenuPage.jsx
+++ src/pages/notes/NotesMenuPage/NotesMenuPage.jsx
@@ -46,32 +46,33 @@ function NotesMenuPage() {
         <div className='menu__categories-container'>
           <h2 className='menu__cat-title'>Categories</h2>
           <ul className='menu__category-list'>
-          <li className='menu__category' onClick={() => setSelectedCategory('all')}>all notes</li>
+            <li className='menu__category' onClick={() => setSelectedCategory('all')}>all notes</li>
             {categories.map((category) => (
               <li className='menu__category' key={category.category_id} onClick={() => setSelectedCategory(category.category_id)}>{category.name}</li>
             ))}
           </ul>
         </div>
         <div className='menu__notes-list'>
+          <h2 className='menu__cat-title'>Notes List</h2>
           {selectedCategory ? (
             notesByCategory.length > 0 ? (
-              <ul>
+              <ul className='notes-list'>
                 {notesByCategory.map((note) => (
-                  <li key={note.note_id} onClick={() => navigate(`/jitsujournal/${note.note_id}`)}>
-                    <h4>{note.title}</h4>
-                    <ul>
+                  <li className='notes-list__item' key={note.note_id} onClick={() => navigate(`/jitsujournal/${note.note_id}`)}>
+                    <h4 className='notes-list__title'>{note.title}</h4>
+                    <ul className='notes-list__tags-container'>
                       {note.tags.map((tag, index) => (
-                        <li key={index}>{tag}</li>
+                        <li className='notes-list__tag' key={index}>{tag}</li>
                       ))}
                     </ul>
                   </li>
                 ))}
               </ul>
             ) : (
-              <p>No notes yet</p>
+              <p className='notes-list__info'>No notes yet</p>
             )
           ) : (
-            <p>Please select a category to view notes</p>
+            <p className='notes-list__info'>Please select a category to view notes</p>
           )}
         </div>
       </main>
