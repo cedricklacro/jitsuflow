281fea231a12adfadef7a16a68d13249f93ab5a0
Cedrick <lacro.cedrick@outlook.com>
2025-02-23 22:19:26 -0700

    Fix category styling for mobile and change tag bg color

--- src/pages/notes/NotesMenuPage/NotesMenuPage.jsx
+++ src/pages/notes/NotesMenuPage/NotesMenuPage.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import PageHeader from '../../../components/PageHeader/PageHeader';
+import editicon from '../../../assets/icons/edit-icon.svg'
 
 function NotesMenuPage() {
   const [categories, setCategories] = useState([]);
@@ -46,7 +47,7 @@ function NotesMenuPage() {
         <div className='menu__categories-container'>
           <h2 className='menu__cat-title'>Categories</h2>
           <ul className='menu__category-list'>
-            <li className='menu__category' onClick={() => setSelectedCategory('all')}>all notes</li>
+            <li className='menu__category' onClick={() => setSelectedCategory('all')}>All Notes</li>
             {categories.map((category) => (
               <li className='menu__category' key={category.category_id} onClick={() => setSelectedCategory(category.category_id)}>{category.name}</li>
             ))}
@@ -59,12 +60,15 @@ function NotesMenuPage() {
               <ul className='notes-list'>
                 {notesByCategory.map((note) => (
                   <li className='notes-list__item' key={note.note_id} onClick={() => navigate(`/jitsujournal/${note.note_id}`)}>
-                    <h4 className='notes-list__title'>{note.title}</h4>
-                    <ul className='notes-list__tags-container'>
-                      {note.tags.map((tag, index) => (
-                        <li className='notes-list__tag' key={index}>{tag}</li>
-                      ))}
-                    </ul>
+                    <div className='notes-list__left-container'>
+                      <h4 className='notes-list__title'>{note.title}</h4>
+                      <ul className='notes-list__tags-container'>
+                        {note.tags.map((tag, index) => (
+                          <li className='notes-list__tag' key={index}>{tag}</li>
+                        ))}
+                      </ul>
+                    </div>
+                    <img className='notes-list__editicon' src={editicon} alt='edit icon' onClick={() => navigate(`/jitsujournal/${note.note_id}/edit`)} />
                   </li>
                 ))}
               </ul>
